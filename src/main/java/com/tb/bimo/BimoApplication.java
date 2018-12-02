d2974110d17f10f6c76b3b845dde9d79175c0f80
Burak Koçer <burak.kocer@d-teknoloji.com.tr>
2018-12-02 16:57:27 +0300

    Initialize spring app

---
+++ src/main/java/com/tb/bimo/BimoApplication.java
@@ -0,0 +1,12 @@
+package com.tb.bimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BimoApplication.class, args);
+	}
+}
